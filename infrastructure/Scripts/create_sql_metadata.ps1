8d197224a6ff1dc572d32e8c1034bf72d111b94b
Samarendra Panda <Samarendra.panda@outlook.com>
2022-08-07 19:00:48 +0530

    adding the Powershell script

---
+++ infrastructure/Scripts/create_sql_metadata.ps1
@@ -0,0 +1,24 @@
+﻿param(
+    [string]$keyVaultname,
+    [string]$keyVaultSecretnameSqlConnString,
+    [string]$scriptPath
+
+)
+# Installing the SQLServer Module
+
+if (!(Get-Module -Name SQLServer)){
+    Write-Output "Installing the SQL Server Module"
+    Install-Module -Name SQLServer
+    Import-Module SQLServer
+}
+
+else{
+Write-Output "SQL Server Module is present"
+}
+
+
+# Get the secret of the sql server Module
+$sqldbConnString = Get-AzKeyVaultSecret -VaultName $keyVaultname -Name $keyVaultSecretnameSqlConnStringb -AsPlainText
+
+# Run the Scripts
+Invoke-SqlCmd -ConnectionString $sqldbConnString  -InputFile $scriptPath
